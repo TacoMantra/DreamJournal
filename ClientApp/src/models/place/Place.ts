317749f2ef629603d9f1e24af742f44ba71a6921
Jon Miller <jmiller@dealerpolicy.com>
2021-02-28 16:22:01 -0500

    place prepositions

--- ClientApp/src/models/place/Place.ts
+++ ClientApp/src/models/place/Place.ts
@@ -1,3 +1,5 @@
+import * as prepositions from '../../consts/terms/prepositions';
+
 export enum PlaceType {
   Unknown = 'Unknown',
   Residence = 'Residence',
@@ -26,9 +28,42 @@ export enum PlaceType {
   LakeOrPond = 'Lake or pond',
   River = 'River',
   Void = 'Void',
-  Heaven = 'Heave',
+  Heaven = 'Heaven',
   Hell = 'Hell',
   Purgatory = 'Purgatory',
+  Other = 'Other',
+}
+
+export enum PlacePreposition {
+  Residence = prepositions.IN_A,
+  Building = prepositions.IN_A,
+  Store = prepositions.AT_THE,
+  PlaceOfWorship = prepositions.IN_A,
+  Courthouse = prepositions.IN_A,
+  School = prepositions.AT,
+  Jail = prepositions.IN,
+  GovernmentBuilding = prepositions.IN_A,
+  Library = prepositions.AT_THE,
+  Castle = prepositions.IN_A,
+  CurrentHome = prepositions.IN_YOUR,
+  PreviousHome = prepositions.IN_YOUR,
+  ChildhoodHome = prepositions.IN_YOUR,
+  Road = prepositions.ON_THE,
+  PathOrTrail = prepositions.ON_A,
+  Garden = prepositions.IN_A,
+  Yard = prepositions.IN_THE,
+  Field = prepositions.IN_A,
+  Mountain = prepositions.ON_A,
+  Forest = prepositions.IN_A,
+  Desert = prepositions.IN_A,
+  Island = prepositions.ON_AN,
+  Ocean = prepositions.IN_THE,
+  LakeOrPond = prepositions.IN_A,
+  River = prepositions.ON_A,
+  Void = prepositions.IN_THE,
+  Heaven = prepositions.IN,
+  Hell = prepositions.IN,
+  Purgatory = prepositions.IN,
   Other = 'Other',
 }
 
